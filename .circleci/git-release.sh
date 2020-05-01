10fa85d9a09aa7b2b2ab3101d784908d3c55dbb5
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-05-01 17:08:33 +0700

    Add Circle CI build.

---
+++ .circleci/git-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+TOKEN="${GitHubReleaseToken}"
+VERSION=1.0.${CIRCLE_BUILD_NUM}
+REPONAME=${CIRCLE_PROJECT_REPONAME}
+USER=${CIRCLE_PROJECT_USERNAME}
+BRANCH=${CIRCLE_BRANCH}
+DESCRIPTION=$1
+REPOURL="https://api.github.com/repos/${USER}/${REPONAME}"
+
+generate_post_data()
+{
+  cat <<EOF
+{
+  "tag_name": "${VERSION}",
+  "target_commitish": "${BRANCH}",
+  "name": "${VERSION}",
+  "body": "${DESCRIPTION}",
+  "draft": false,
+  "prerelease": false
+}
+EOF
+}
+
+echo $(generate_post_data)
+
+wget -q --post-data="$(generate_post_data)" ${REPOURL}/releases?access_token=${TOKEN} -O release.json
