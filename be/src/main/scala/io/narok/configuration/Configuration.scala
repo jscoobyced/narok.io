473b1d136d15fd4e6326b971cdda1b0c598ca350
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-05-30 19:49:08 +0700

    Try to fix Configuration file not found on CircelCI

--- be/src/main/scala/io/narok/configuration/Configuration.scala
+++ be/src/main/scala/io/narok/configuration/Configuration.scala
@@ -5,6 +5,7 @@ import java.io.{File, FileOutputStream}
 import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.annotation.tailrec
+import scala.util.{Failure, Success, Try}
 
 object Configuration {
   @tailrec
@@ -12,17 +13,23 @@ object Configuration {
     val applicationConf = new File(s"./conf/$configuration")
     if (applicationConf.exists()) ConfigFactory.parseFile(applicationConf)
     else {
-      val configBytes = ClassLoader
-        .getSystemResourceAsStream(s"$configuration")
-        .readAllBytes()
-      val parentDirectory = applicationConf.getParentFile
-      if (!parentDirectory.exists()) {
-        parentDirectory.mkdir()
+      Try(
+        ClassLoader
+          .getSystemResourceAsStream(s"$configuration")) match {
+        case Success(inputStream) =>
+          val configBytes     = inputStream.readAllBytes()
+          val parentDirectory = applicationConf.getParentFile
+          if (!parentDirectory.exists()) {
+            parentDirectory.mkdir()
+          }
+          applicationConf.createNewFile()
+          val fos = new FileOutputStream(applicationConf)
+          fos.write(configBytes)
+          getConfig(configuration)
+        case Failure(error: Throwable) =>
+          println(s"Configuration file not found: ${error.getMessage}")
+          ConfigFactory.empty()
       }
-      applicationConf.createNewFile()
-      val fos = new FileOutputStream(applicationConf)
-      fos.write(configBytes)
-      getConfig(configuration)
     }
   }
 }
