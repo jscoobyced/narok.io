f904dfc42dc002a2be3b51508163134a09ef1d69
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2020-07-11 20:44:14 +0700

    Fix duplicate id in mock

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -14,9 +14,11 @@ if (mode === 'development') {
   const articles: Article[] = [];
   const lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies ut augue eu imperdiet. Donec tincidunt congue tortor, eu finibus ex facilisis ut. Etiam aliquam augue non magna commodo auctor. Phasellus nec tellus egestas, gravida ipsum nec, semper augue. In facilisis quam purus, congue egestas augue gravida at. Aenean et justo placerat, ultrices nisi sit amet, molestie risus. Duis eu nulla eget nisi interdum interdum. Curabitur ornare vel odio rhoncus aliquet. Praesent luctus rutrum pharetra. Nullam sed eros pharetra urna porttitor viverra. Nulla facilisi. Sed molestie sodales rhoncus. Cras gravida porttitor nulla ac suscipit. Suspendisse sed neque nunc. Praesent ac varius nisi. Donec suscipit elit sit amet eros scelerisque tempor. Curabitur bibendum turpis id rhoncus ultricies. Maecenas hendrerit finibus felis, ac lobortis ex vestibulum sit amet. Donec condimentum aliquet nulla, sed scelerisque orci. Phasellus massa felis, maximus eget dolor ut, dapibus semper ante.";
   const contents = [toBlogContentText(lorem)];
-  const textArticle: Article = toArticle(1, 'Coming soon...', contents, '2020-05-04 22:52');
-  articles.push(textArticle);
-  articles.push(textArticle);
+  const textArticle1: Article = toArticle(1, 'Coming soon...', contents, '2020-05-04 22:52');
+  articles.push(textArticle1);
+  const textArticle2 = { ...textArticle1};
+  textArticle2.id = 2;
+  articles.push(textArticle2);
   httpService = new HttpServiceMock(articles);
 } else {
   httpService = new HttpService();
